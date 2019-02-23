683778ebe54b383086798dd4c4592ecd39844dcd
sk-surya <45133775+sk-surya@users.noreply.github.com>
2019-02-22 21:57:59 -0600

    Works for higher dimensions now. Singular matrix exceptions is not handled yet (for jacobian inverse).

--- UCO.py
+++ UCO.py
@@ -6,11 +6,12 @@ import numpy
 
 # input : function variables from user (as space separated string)
 # function variables 'f_vars'
-f_vars_string = 'x'
+f_vars_string = 'x1 x2'
 # input : function input from user (as normal python expression using the above variables)
-f_string = '(x-3)*((x-5)**2)*(3*x-16)'
-# x1**3 + 5*x1**2*x2 + 7*x1*x2**2 + 2*x2**3
-
+#f_string = '(x-3)*((x-5)**2)*(3*x-16)'
+f_string = 'x1**3 + 5*x1**2*x2 + 7*x1*x2**2 + 2*x2**3'
+# input : intial point x0
+x0 = [2,1]
 var_str_list = f_vars_string.split()
 # print(type(var_str_list))
 # var_list is a list of strings above
@@ -44,7 +45,8 @@ def hessian(function):
         for y in ab.keys():
             h[it] = sympy.diff(function, x, y)
             it += 1
-    return numpy.matrix(list(mat(h)))
+    hess = numpy.matrix(list(mat(h)))
+    return hess
 
 def jacobian(function):
     return numpy.transpose(hessian(function))
@@ -60,7 +62,7 @@ def evaluate(grid, x):
     #replacements = {key: value for (key, value) in zip}
     xz = list(itertools.chain.from_iterable(x.tolist()))
     replacements = dict(zip(var_str_list, xz))
-    evaluator = lambda t: t.evalf(subs=replacements)
+    evaluator = lambda t: float(t.evalf(subs=replacements))
     vfunc = numpy.vectorize(evaluator)
     out_grid = vfunc(grid)
     return out_grid
@@ -72,6 +74,7 @@ def improve(old_x, grad, jaco):
         j_inv = 1/j_x0.item(0)
         g = g_x0.item(0)
     else:
+        g = g_x0
         j_inv = numpy.linalg.inv(j_x0)
     new_x = old_x - numpy.dot(j_inv, g)
     return new_x
@@ -84,10 +87,10 @@ def minimize_NR(function, x0):
     i = 0
     gap = 1
     new_x = 'inf'
-    while(gap > 0.00001):
+    while(gap > 0.001):
         old_x = x[i]
         new_x = improve(old_x, grad, jaco)
-        gap = new_x - old_x
+        gap = numpy.linalg.norm(new_x - old_x)
         x.append(new_x)
         i += 1
     return new_x
@@ -95,9 +98,10 @@ def minimize_NR(function, x0):
 
 # Parse the input function string as SymPy expression
 f = parse_expr(f_string)
-f = sympy.integrate(f)
+# Uncomment the following line if you want to find the roots of the given function (for 1D functions)
+#f = sympy.integrate(f)
 print(sympy.expand(f))
-x0 = [1]
+
 ans = minimize_NR(f, x0)
 print('ans is: ', ans)
 print('gradient at ', ans, 'is ', evaluate(gradient(f), ans))
